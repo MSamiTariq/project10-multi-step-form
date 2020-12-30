335be0289a172c03bab557221cf9d3497d111e9c
samitariq12 <samitariq40@gmail.com>
2020-12-30 12:27:48 +0500

    updated

--- src/Forms/ContactInformation.tsx
+++ src/Forms/ContactInformation.tsx
@@ -3,137 +3,142 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import { Grid, TextField, Button } from "@material-ui/core";
 import * as Yup from "yup";
 import TextError from "./TextError";
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
 interface Props {
-  submit: React.Dispatch<React.SetStateAction<number>>;
-  setFormValues: React.Dispatch<React.SetStateAction<{}>>;
-  prevValues: any;
+	submit: React.Dispatch<React.SetStateAction<number>>;
+	setFormValues: React.Dispatch<React.SetStateAction<{}>>;
+	prevValues: any;
 }
 
 interface accountInfo {
-  emailID: string;
-  userName: string;
-  password: string;
-  confirmPassword: string;
+	emailID: string;
+	userName: string;
+	password: string;
+	confirmPassword: string;
 }
 
-
-
-
 const validationSchema = Yup.object({
-  emailID: Yup.string().required("Required").email("Invalid Email"),
-  userName: Yup.string().required("Required"),
-  password: Yup.string()
-    .required("Required")
-    .min(6, "Password is too shot")
-    .max(15, "Password is to long"),
-  confirmPassword: Yup.string()
-    .required("Required")
-    .oneOf([Yup.ref("password")], "Password is not matching"),
+	emailID: Yup.string().required("Required").email("Invalid Email"),
+	userName: Yup.string().required("Required"),
+	password: Yup.string()
+		.required("Required")
+		.min(6, "Password is too shot")
+		.max(15, "Password is to long"),
+	confirmPassword: Yup.string()
+		.required("Required")
+		.oneOf([Yup.ref("password")], "Password is not matching"),
 });
 
 const AccountInformation: FC<Props> = ({
-  submit,
-  setFormValues,
-  prevValues,
+	submit,
+	setFormValues,
+	prevValues,
 }) => {
+	const [showPassword, setShowPassword] = React.useState<boolean>(false);
 
-const [showPassword, setShowPassword] = React.useState<boolean>(false);
+	return (
+		<Formik
+			initialValues={prevValues}
+			validationSchema={validationSchema}
+			onSubmit={(values: accountInfo) => {
+				setFormValues({ ...values, ...prevValues });
+				submit(2);
+			}}
+		>
+			{(formik) => {
+				return (
+					<Form className="form_Content" autoComplete="off">
+						<h1>Contact Information</h1>
+						<Grid container spacing={3} justify="center">
+							<Grid item sm={8} xs={10}>
+								<Field
+									name="emailID"
+									as={TextField}
+									label="E-mail ID"
+									variant="outlined"
+									helperText={
+										<ErrorMessage name="emailID" component={TextError} />
+									}
+									fullWidth
+								/>
+							</Grid>
 
+							<Grid item sm={8} xs={10}>
+								<Field
+									name="userName"
+									as={TextField}
+									label="User Name"
+									variant="outlined"
+									helperText={
+										<ErrorMessage name="userName" component={TextError} />
+									}
+									fullWidth
+								/>
+							</Grid>
 
-  return (
-    <Formik
-      initialValues={prevValues}
-      validationSchema={validationSchema}
-      onSubmit={(values: accountInfo) => {
-        setFormValues({ ...values, ...prevValues });
-        submit(2);
-      }}
-    >
-      {(formik) => {
-        return (
-          <Form className="form_Content" autoComplete = 'off'>
-            <h1>Account Information</h1>
-            <Grid container spacing={3} justify="center">
-              <Grid item sm={8} xs={10}>
-                <Field
-                  name="emailID"
-                  as={TextField}
-                  label="E-mail ID"
-                  variant="outlined"
-                  helperText={
-                    <ErrorMessage name="emailID" component={TextError} />
-                  }
-                  fullWidth
-                />
-              </Grid>
+							<Grid item sm={8} xs={10}>
+								<Field
+									name="password"
+									as={TextField}
+									label="Password"
+									variant="outlined"
+									type={showPassword ? "text" : "password"}
+									helperText={
+										<ErrorMessage name="password" component={TextError} />
+									}
+									fullWidth
+								/>
+								<button
+									onClick={(e) => {
+										e.preventDefault();
+										setShowPassword(!showPassword);
+									}}
+								>
+									{showPassword ? <Visibility /> : <VisibilityOff />}
+								</button>
+							</Grid>
 
-              <Grid item sm={8} xs={10}>
-                <Field
-                  name="userName"
-                  as={TextField}
-                  label="User Name"
-                  variant="outlined"
-                  helperText={
-                    <ErrorMessage name="userName" component={TextError} />
-                  }
-                  fullWidth
-                />
-              </Grid>
-
-              <Grid item sm={8} xs={10}>
-                <Field
-                  name="password"
-                  as={TextField}
-                  label="Password"
-                  variant="outlined"
-                  type = {showPassword? "text" : "password"}
-                  helperText={
-                    <ErrorMessage name="password" component={TextError} />
-                  }
-                  fullWidth
-                />
-                <button onClick={(e) =>{
-                  e.preventDefault();
-                  setShowPassword(!showPassword)}}>{showPassword ? <VisibilityOff /> : <Visibility />}</button>
-              </Grid>
-
-              <Grid item sm={8} xs={10}>
-                <Field
-                  name="confirmPassword"
-                  as={TextField}
-                  label="Confirm Password"
-                  variant="outlined"
-                  type = {showPassword? "text" : "password"}
-                  helperText={
-                    <ErrorMessage
-                      name="confirmPassword"
-                      component={TextError}
-                    />
-                  }
-                  fullWidth
-                />
-              </Grid>
-            </Grid>
-            <div className= 'buttons'>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => submit(0)}
-              >
-                Back
-              </Button>
-              <Button variant="contained" color="primary" type="submit">
-                Next
-              </Button>
-            </div>
-          </Form>
-        );
-      }}
-    </Formik>
-  );
+							<Grid item sm={8} xs={10}>
+								<Field
+									name="confirmPassword"
+									as={TextField}
+									label="Confirm Password"
+									variant="outlined"
+									type={showPassword ? "text" : "password"}
+									helperText={
+										<ErrorMessage
+											name="confirmPassword"
+											component={TextError}
+										/>
+									}
+									fullWidth
+								/>
+							</Grid>
+						</Grid>
+						<div className="buttons">
+							<Button
+								variant="contained"
+								color="primary"
+								onClick={() => submit(0)}
+							>
+								Back
+							</Button>
+							<Button
+								variant="contained"
+								color="primary"
+								type="submit"
+								disabled={!formik.dirty || !formik.isValid}
+							>
+								Next
+							</Button>
+						</div>
+					</Form>
+				);
+			}}
+		</Formik>
+	);
 };
 
 export default AccountInformation;
--- src/Stepper/index.tsx
+++ src/Stepper/index.tsx
@@ -2,152 +2,161 @@ import React, { useState } from "react";
 import PersonalInformation from "../Forms/PersonalInformation";
 import AccountInformation from "../Forms/ContactInformation";
 import Review from "../Forms/Review";
-import { makeStyles, Theme, createStyles, withStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import EmailIcon from '@material-ui/icons/Email';
-import InfoIcon from '@material-ui/icons/Info';
-import RateReviewIcon from '@material-ui/icons/RateReview';
-import StepConnector from '@material-ui/core/StepConnector';
-import { StepIconProps } from '@material-ui/core/StepIcon';
-
+import {
+	makeStyles,
+	Theme,
+	createStyles,
+	withStyles,
+} from "@material-ui/core/styles";
+import clsx from "clsx";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import EmailIcon from "@material-ui/icons/Email";
+import InfoIcon from "@material-ui/icons/Info";
+import RateReviewIcon from "@material-ui/icons/RateReview";
+import StepConnector from "@material-ui/core/StepConnector";
+import { StepIconProps } from "@material-ui/core/StepIcon";
 
 const ColorlibConnector = withStyles({
-  alternativeLabel: {
-    top: 22,
-  },
-  active: {
-    '& $line': {
-      backgroundImage:
-        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
-    },
-  },
-  completed: {
-    '& $line': {
-      backgroundImage:
-        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
-    },
-  },
-  line: {
-    height: 3,
-    border: 0,
-    backgroundColor: '#eaeaf0',
-    borderRadius: 1,
-  },
+	alternativeLabel: {
+		top: 22,
+	},
+	active: {
+		"& $line": {
+			backgroundImage:
+				"linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)",
+		},
+	},
+	completed: {
+		"& $line": {
+			backgroundImage:
+				"linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)",
+		},
+	},
+	line: {
+		height: 3,
+		border: 0,
+		backgroundColor: "#eaeaf0",
+		borderRadius: 1,
+	},
 })(StepConnector);
 
 const useColorlibStepIconStyles = makeStyles({
-  root: {
-    backgroundColor: '#ccc',
-    zIndex: 1,
-    color: '#fff',
-    width: '50px',
-    height: 50,
-    display: 'flex',
-    borderRadius: '50%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  active: {
-    backgroundImage:
-      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
-    boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
-  },
-  completed: {
-    backgroundImage:
-      'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
-  },
+	root: {
+		backgroundColor: "#ccc",
+		zIndex: 1,
+		color: "#fff",
+		width: "50px",
+		height: 50,
+		display: "flex",
+		borderRadius: "50%",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	active: {
+		backgroundImage:
+			"linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
+		boxShadow: "0 4px 10px 0 rgba(0,0,0,.25)",
+	},
+	completed: {
+		backgroundImage:
+			"linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
+	},
 });
 
 function ColorlibStepIcon(props: StepIconProps) {
-  const classes = useColorlibStepIconStyles();
-  const { active, completed } = props;
+	const classes = useColorlibStepIconStyles();
+	const { active, completed } = props;
 
-  const icons: { [index: string]: React.ReactElement } = {
-    1: <InfoIcon />,
-    2: <EmailIcon />,
-    3: <RateReviewIcon />,
-  };
+	const icons: { [index: string]: React.ReactElement } = {
+		1: <InfoIcon />,
+		2: <EmailIcon />,
+		3: <RateReviewIcon />,
+	};
 
-  return (
-    <div
-      className={clsx(classes.root, {
-        [classes.active]: active,
-        [classes.completed]: completed,
-      })}
-    >
-      {icons[String(props.icon)]}
-    </div>
-  );
+	return (
+		<div
+			className={clsx(classes.root, {
+				[classes.active]: active,
+				[classes.completed]: completed,
+			})}
+		>
+			{icons[String(props.icon)]}
+		</div>
+	);
 }
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%',
-    },
-    button: {
-      marginRight: theme.spacing(1),
-    },
-    instructions: {
-      marginTop: theme.spacing(1),
-      marginBottom: theme.spacing(1),
-    },
-  }),
+	createStyles({
+		root: {
+			width: "100%",
+		},
+		button: {
+			marginRight: theme.spacing(1),
+		},
+		instructions: {
+			marginTop: theme.spacing(1),
+			marginBottom: theme.spacing(1),
+		},
+	})
 );
 
 function getSteps() {
-  return ['Persnal Information', 'Contact Information', 'Review'];
+	return ["Persnal Information", "Contact Information", "Review"];
 }
 
 function getStepContent(
-  stepIndex: number,
-  setStepIndex: React.Dispatch<React.SetStateAction<number>>,
-  setFormValues: React.Dispatch<React.SetStateAction<{}>>,
-  formValues: {}
+	stepIndex: number,
+	setStepIndex: React.Dispatch<React.SetStateAction<number>>,
+	setFormValues: React.Dispatch<React.SetStateAction<{}>>,
+	formValues: {}
 ) {
-  switch (stepIndex) {
-    case 0:
-      return (
-        <PersonalInformation
-          submit={setStepIndex}
-          prevValues={formValues}
-          setFormValues={setFormValues}
-        />
-      );  
-    case 1:
-      return (
-        <AccountInformation
-          submit={setStepIndex}
-          prevValues={formValues}
-          setFormValues={setFormValues}
-        />
-      );
-    case 2:
-      return <Review submit={setStepIndex} formValues={formValues} />;
-    default:
-      return "Unknown stepIndex";
-  }
+	console.log(formValues);
+	switch (stepIndex) {
+		case 0:
+			return (
+				<PersonalInformation
+					submit={setStepIndex}
+					prevValues={formValues}
+					setFormValues={setFormValues}
+				/>
+			);
+		case 1:
+			return (
+				<AccountInformation
+					submit={setStepIndex}
+					prevValues={formValues}
+					setFormValues={setFormValues}
+				/>
+			);
+		case 2:
+			return <Review submit={setStepIndex} formValues={formValues} />;
+		default:
+			return "Unknown stepIndex";
+	}
 }
 
 export default function CustomizedSteppers() {
-  const classes = useStyles();
-  const [activeStep, setActiveStep] = useState(0);
-  const [formValues, setFormValues] = useState({});
-  const steps = getSteps();
+	const classes = useStyles();
+	const [activeStep, setActiveStep] = useState(0);
+	const [formValues, setFormValues] = useState({});
+	const steps = getSteps();
 
-  return (
-    <div className={classes.root}>
-      <Stepper alternativeLabel activeStep={activeStep} connector={<ColorlibConnector />}>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      {getStepContent(activeStep, setActiveStep, setFormValues, formValues)}
-    </div>
-  );
+	return (
+		<div className={classes.root}>
+			<Stepper
+				alternativeLabel
+				activeStep={activeStep}
+				connector={<ColorlibConnector />}
+			>
+				{steps.map((label) => (
+					<Step key={label}>
+						<StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
+					</Step>
+				))}
+			</Stepper>
+			{getStepContent(activeStep, setActiveStep, setFormValues, formValues)}
+		</div>
+	);
 }
